a375795172c026277d291740ff4e31c6864bfea6
amonratCha <amonrat.cha@northbkk.ac.th>
2017-10-31 16:02:53 +0700

    productdetail

--- src/pages/product-detail/product-detail.ts
+++ src/pages/product-detail/product-detail.ts
@@ -14,20 +14,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'product-detail.html',
 })
 export class ProductDetailPage {
+  rate = 3.5;
 
   dataDescriptions = {
-    
-      "name": "Ancient Coffee",
-      "image": [
-        "http://gabrielserafini.com/wp-content/uploads/IMG_3139-1000x1000.jpg",
-        "http://gabrielserafini.com/wp-content/uploads/IMG_3139-1000x1000.jpg",
-        "http://gabrielserafini.com/wp-content/uploads/IMG_3139-1000x1000.jpg"
-      ],
-      "detail": "The origin and history of coffee dates back to the 10th century, and possibly earlier with a number of reports and legends surrounding its first use. The native (undomesticated) origin of coffee is thought to have been Ethiopia. The earliest substantiated evidence of either coffee drinking or knowledge of the coffee tree is from the 15th century, in the Sufi monasteries of Yemen.",
-      "price": 300,
-      "normalprice": 500,
-      "discount": 10,
-      "discounttype": "%"
+
+    "name": "Cappuccino",
+    "image": [
+      "./assets/image/coffee-1.jpg",
+      "./assets/image/coffee-2.jpg",
+      "./assets/image/coffee-3.jpg"
+    ],
+    "detail": "The origin and history of coffee dates back to the 10th century, and possibly earlier with a number of reports and legends surrounding its first use. The native (undomesticated) origin of coffee is thought to have been Ethiopia. The earliest substantiated evidence of either coffee drinking or knowledge of the coffee tree is from the 15th century, in the Sufi monasteries of Yemen.",
+    "price": 300,
+    "normalprice": 500,
+    "discount": 10,
+    "discounttype": "%"
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
