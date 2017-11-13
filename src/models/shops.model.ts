29053056651fa3f1de44ca67f43d99b8327f7a04
amonratCha <amonrat.cha@northbkk.ac.th>
2017-11-13 15:02:55 +0700

    xxxxxx

--- src/models/shops.model.ts
+++ src/models/shops.model.ts
@@ -21,3 +21,48 @@ export class ShopsModel {
     user: UsersModel;
     logo: string;
 }
+
+////////////////////////////////////////////////////////////
+export class ShopsModel2 {
+    _id: string;
+    name: string;
+    address: addressModel = new addressModel() ;
+    shopcode: string;
+    email: string;
+    phone: string;
+    location: locateModel = new locateModel();
+    created: Date;
+    logo: string;
+    shopowner: string;
+    openinghours: openTimeModel = new openTimeModel;
+    facebook: string;
+    instagram: string;
+    line: string;
+    parking: string;
+    favorite: Array<UsersModel>;
+    image: Array<string>;
+    createduser: UsersModel = new UsersModel();
+    editlog: Array<createUserModel>
+}
+export class createUserModel {
+    user: UsersModel;
+    date: Date;
+}
+export class openTimeModel{
+    open: Date;
+    close: Date;
+}
+export class addressModel{
+    address: string;
+    subdistrict: string;
+    district: string;
+    province: string;
+    postcode: string;
+}
+
+export class locateModel{
+    
+        lat: string;
+        lng: string;
+    
+}
--- src/pages/shop-location/shop-location.ts
+++ src/pages/shop-location/shop-location.ts
@@ -1,6 +1,10 @@
+import { ShopsModel2 } from '../../models/shops.model';
+import { LocationProvider } from '../../providers/location/location';
 
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMaps, GoogleMap, Geocoder, GoogleMapsEvent, GoogleMapOptions, CameraPosition, MarkerOptions, Marker, LatLng } from '@ionic-native/google-maps';
 
 declare var google;
 
@@ -10,19 +14,37 @@ declare var google;
   templateUrl: 'shop-location.html',
 })
 export class ShopLocationPage {
-  
-  @ViewChild('map') mapElement: ElementRef;
+  dataShop: any = [];
   private latLng: any = {};
-  dataShop;
-
+  map: GoogleMap;
+  public dataClick;
+  
+  // @ViewChild('map') mapElement: ElementRef;
+  // private latLng: any = {};
+  // dataShop;
   Edit = "map";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mapsLatlong: Array<ShopsModel2> = new Array<ShopsModel2>();
 
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public locationProvider:LocationProvider,
+    public platform: Platform,
+    public geolocation: Geolocation,
+  ) {
+    
   }
 
   ionViewDidLoad() {
-    this.initMap();
+    // this.initMap();
+    console.log('ionViewDidLoad MapPage');
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
+    this.gitDataShopLocations();
+
     
   }
 
@@ -36,48 +58,129 @@ export class ShopLocationPage {
     }
   }
 
-  initMap() {
-    this.latLng = {
-      lat: 13.9381232,
-      lng: 100.71376
-    };
-
-    let map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 12,
-      center: this.latLng
-    });
-
-    let request = {
-      location: this.latLng,
-      radius: '112500',
-      types: ['restaurant'],
-      keyword: 'coffee'
-    };
-
-    let service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, (results, status) => {
-      if (status == 'OK') {
-        results.forEach(element => {
-          let marker = new google.maps.Marker({
-            draggable: false,
-            position: element.geometry.location,
-            map: map,
-            icon:"./assets/image/coffee-n-tea.png"
-            // icon:  {
-            //   url: '../../assets/image/map-pin-746123_1920.png',
-            //   anchor: new google.maps.Point(10, 10),
-            //   scaledSize: new google.maps.Size(30, 50)
-            // }
+  gitDataShopLocations(){
+    this.locationProvider.shopLocation().then((res)=>{
+      console.log(res);
+      this.mapsLatlong = res;
+      console.log(this.mapsLatlong[0]._id);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+  
+  loadMap() {
+    
+        this.geolocation.getCurrentPosition().then((resp) => {
+    
+          let location = new LatLng(resp.coords.latitude, resp.coords.longitude);
+          this.map = new GoogleMap('map', {
+            'controls': {
+              'compass': true,
+              'myLocationButton': true,
+              'indoorPicker': true,
+              'zoom': true
+            },
+            'gestures': {
+              'scroll': true,
+              'tilt': true,
+              'rotate': true,
+              'zoom': true
+            },
+            'camera': {
+              'target': location,
+              'tilt': 90,
+              'zoom': 14,
+              'bearing': 30
+            },
           });
+    
+          this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            alert('Map is ready!');
+            let latlng = {
+              lat: 0,
+              lng: 0
+            };
+            for (let i = 0; i < this.mapsLatlong.length; i++) {
+              let addmarker = this.map.addMarker({
+                title: 'select shop',
+                icon: './assets/icon/pin_2.png',
+                animation: 'BOUNCE',
+                position: {
+                  lat: this.mapsLatlong[i].location[i].lat,
+                  lng: this.mapsLatlong[i].location[i].lng
+                }
+              })
+                .then(marker => {
+                  marker.on(GoogleMapsEvent.MARKER_CLICK)
+                    .subscribe((data) => {
+                      latlng = { lat: data[0].lat, lng: data[0].lng };
+                      alert(JSON.stringify(latlng));
 
-          google.maps.event.addListener(marker, 'click', () => {
-            alert(JSON.stringify(element));
-            this.dataShop = element;
-            console.log(this.dataShop);
+                      // this.nativeGeocoder.reverseGeocode(data[0].lat, data[0].lng)
+                      // .then((result: NativeGeocoderReverseResult) => alert(JSON.stringify(result)))
+                      // .catch((error: any) => console.log(error));
+
+
+            
+                    });
+                });
+            }
+           
           });
+        }).catch((error) => {
+          console.log('Error getting location', error);
         });
       }
-    });
-  }
 
 }
+  // initMap() {
+  //   this.latLng = {
+  //     lat: 13.9381232,
+  //     lng: 100.71376
+  //   };
+
+  //   let map = new google.maps.Map(this.mapElement.nativeElement, {
+  //     zoom: 12,
+  //     center: this.latLng
+  //   });
+
+  //   let request = {
+  //     location: this.latLng,
+  //     radius: '112500',
+  //     types: ['restaurant'],
+  //     keyword: 'coffee'
+  //   };
+
+    
+  //   let service = new google.maps.places.PlacesService(map);
+  //   service.nearbySearch(request, (results, status) => {
+  //     if (status == 'OK') {
+  //       results.forEach(element => {
+  //         let marker = new google.maps.Marker({
+  //           draggable: false,
+  //           // position: element.geometry.location,
+  //           // position:{
+  //           //   lat: this.shoplatlong[i].location.lat,
+  //           //   lng: this.shoplatlong[i].location.lng
+  //           // },
+  //           map: map,
+  //           icon:"./assets/image/coffee-n-tea.png"
+  //           // icon:  {
+  //           //   url: '../../assets/image/map-pin-746123_1920.png',
+  //           //   anchor: new google.maps.Point(10, 10),
+  //           //   scaledSize: new google.maps.Size(30, 50)
+  //           // }
+  //         });
+
+  //         google.maps.event.addListener(marker, 'click', () => {
+  //           alert(JSON.stringify(element));
+  //           this.dataShop = element;
+  //           console.log(this.dataShop);
+  //         });
+  //       });
+  //     }
+  //   });
+  
+  // }
+
+
